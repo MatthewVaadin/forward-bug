4db997791ee3668b9237d95becf28ec4be067125
Matthew Wilson <matthew_w@vaadin.com>
2024-08-16 12:26:59 +0300

    Initial commit

---
+++ src/main/java/com/example/application/views/second/SecondView.java
@@ -0,0 +1,43 @@
+package com.example.application.views.second;
+
+import com.example.application.views.MainLayout;
+import com.example.application.views.first.FirstView;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
+@PageTitle("Second")
+@Route(value = "second", layout = MainLayout.class)
+public class SecondView extends VerticalLayout implements BeforeEnterObserver {
+
+    public SecondView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        event.forwardTo(FirstView.class);
+    }
+
+}
